7fd76805b85033c22d4cb4181e3035f225c8dbcf
LiamAspell <liamaspell123@gmail.com>
2022-10-27 17:00:42 +0100

    Some work on the player done

--- src/Pages/Player.js
+++ src/Pages/Player.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import videoEg from "../resources/gifs/bedroom.gif"
 
 export const Player = () => {
   return (
-    <div>
-      <header className='Player-header'>
-        <h1> Player </h1>
-      </header>
+    <div className='Overlay'>
+      <div className="player">
+        <img src={videoEg} width="100%" height="40%" autoPlay loop muted />
+        <div className="content">
+          <h1>This is the lyrics</h1>
+          <p>adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw adoidhoiha aio dhwioah iodhaiah doihaoi haiowhd iohaiodh oiahwdio haiodh ioahodi haw </p>
+        </div>
+
+      </div>
     </div>
   )
 
